a39082cd6aac146b9dc29af4a5bab3f5c63f5983
Alexis Medina Pérez (UTP) <u23259212@utp.edu.pe>
2024-08-29 21:21:49 -0500

    Nueva carga

--- src/programas/Programa04.java
+++ src/programas/Programa04.java
@@ -2,11 +2,39 @@ package programas;
 
 import java.util.Scanner;
 
+//Crear un programa en Java que permita ingresar la velocidad de un vehículo
+//Si la velocidad es mayor a 80 km/h mostrar un mensaje de "¡ALERTA! Reduzca la velocidad"
+//Si la velocidad es menos o igual a 80 km/h mostrar un mensaje de "Mantenga la velocidad"
+
 public class Programa04 {
     
     public static void main(String[] args) {
         
+        //Declarar variables
+        Double velocidad, NuevaVelocidad;
+        String mensaje;
+        Scanner lectura = new Scanner(System.in);
         
+        //Lectura de datos
+        System.out.print("Ingrese la velocidad del vehículo: ");
+        velocidad = lectura.nextDouble();
+        
+        //Proceso de datos
+        mensaje = "";
+        if (velocidad > 80) {
+        
+            NuevaVelocidad = velocidad - 80;
+            mensaje = "¡ALERTA! Reduzca " + NuevaVelocidad + " km/h su velocidad.";
+        
+        }
+        if (velocidad <= 80) {
+            
+            mensaje = "Mantenga la velocidad";            
+        
+        }
+        
+        //Salida de datos
+        System.out.println(mensaje);
         
     }
     
